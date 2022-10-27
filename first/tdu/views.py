7e719b5d28b7e940d7457c61b9ed1e6587e30991
sunisha <sunisha3@gmail.com>
2022-10-26 21:48:18 -0700

    first commit

---
+++ first/tdu/views.py
@@ -0,0 +1,30 @@
+from .forms import TodoForm
+from .models import Todo
+from django.shortcuts import redirect, render
+def index(request):
+    form = TodoForm()
+    todos = Todo.objects.all()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = TodoForm()
+            return redirect('index')
+    return render(request,'index.html',{'form':form,'todo':todos})
+def delete(request,todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('index')
+def update(request,todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    form = TodoForm(instance=todo)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('index') 
+    return render(request,'update.html',{'form':form})               
+
+
+
